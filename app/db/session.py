b52798683c87267ce1791802d45a4e82e9816235
wfeel <githubwfeel@gmail.com>
2026-05-06 17:42:18 +0300

    move db/ and models/ folder to the app/ folder

---
+++ app/db/session.py
@@ -0,0 +1,13 @@
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import settings
+
+
+# psql engine
+engine = create_engine(settings.database_url)
+Session = sessionmaker(engine)
+
+# use the session
+# with Session() as session:
+    # session.add(some_object)
